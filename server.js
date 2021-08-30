e0b89f5e63ba2cf6309bc49cbfcba7739c107268
Gugnu <lochammehtab@gmail.com>
2021-08-29 23:46:59 -0400

    hosted on heroku

--- server.js
+++ server.js
@@ -44,16 +44,30 @@ function filterByQuery(query, animalsArray) {
     }
     // return the filtered results:
     return filteredResults;
-  }
+}
 
-  app.get('/api/animals', (req, res) => {
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
+app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
       results = filterByQuery(req.query, results);
     }
     res.json(results);
-  });
+});
 
-  app.listen(PORT, () => {
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-  });
+});
